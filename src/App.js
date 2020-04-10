394ef619ad75ea20c5d44d3c73fe46c1cc10cbde
Andy Jiang <andy.jiang99@outlook.com>
2020-04-10 01:12:59 -0400

    fixing errors

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ import Email from './Icons/Email_Logo.svg';
 import PinkCloud from './Icons/Pink_Cloud.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { Picture } from 'react-responsive-picture';
 
 function App() {
   return (
     <div className="App">     
             <Row>
-              <Col sm={4}><img src={Logo} style={{width: "100%", height:"auto"}}/><h1>Accelerating Student Tech Careers</h1></Col>
-              <Col sm={8}><img src={BlueCloud} style={{width: "100%", height:"auto"}}/></Col>
+              <Col sm={4}><img src={Logo} style={{width: "100%", height:"auto"}} alt=""/><h1>Accelerating Student Tech Careers</h1></Col>
+              <Col sm={8}><img src={BlueCloud} style={{width: "100%", height:"auto"}} alt=""/></Col>
             </Row>
             <Row>
-              <Col sm={4}><img src ={Facebook}></img><img src ={LinkedIn}></img><img src ={Email}></img></Col>
+              <Col sm={4}><img src ={Facebook} alt=""></img><img src ={LinkedIn} alt=""></img><img src ={Email} alt=""></img></Col>
             </Row>
             <Row>
-              <Col sm={6}><img src={PinkCloud} style={{width: "100%", height:"auto"}}/></Col>
+              <Col sm={6}><img src={PinkCloud} style={{width: "100%", height:"auto"}}  alt=""/></Col>
             </Row>
     </div>
   );
