3df95d7006ec76e58c3697bc9064f720986e140b
Andy Jiang <andy.jiang99@outlook.com>
2020-05-22 00:07:14 -0400

    Updates

---
+++ precache-manifest.53189628cb8e7893d7e98b69c650ec74.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a4bee6b863cbfe88db5070451351bd",
+    "url": "/SprintUofT/index.html"
+  },
+  {
+    "revision": "8f0e6b3c899cdd32c95f",
+    "url": "/SprintUofT/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b53fd867dc90345a10e2",
+    "url": "/SprintUofT/static/css/main.d5345fe6.chunk.css"
+  },
+  {
+    "revision": "8f0e6b3c899cdd32c95f",
+    "url": "/SprintUofT/static/js/2.3eebf5f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SprintUofT/static/js/2.3eebf5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53fd867dc90345a10e2",
+    "url": "/SprintUofT/static/js/main.fab237e3.chunk.js"
+  },
+  {
+    "revision": "4cf383992ced4cbf54bf",
+    "url": "/SprintUofT/static/js/runtime-main.5fcd2cdd.js"
+  },
+  {
+    "revision": "cfdf1e9cd8dbaea242019a6a230a8da5",
+    "url": "/SprintUofT/static/media/Blue_Cloud.cfdf1e9c.svg"
+  },
+  {
+    "revision": "64784f4cebbcdd81519d8c69db183c75",
+    "url": "/SprintUofT/static/media/Email_Logo.64784f4c.svg"
+  },
+  {
+    "revision": "c6c3fd72131282888f78aa14cc2d514c",
+    "url": "/SprintUofT/static/media/Facebook_Logo.c6c3fd72.svg"
+  },
+  {
+    "revision": "976b193e4189088a6b2dd58fdc78d706",
+    "url": "/SprintUofT/static/media/LinkedIn_Logo.976b193e.svg"
+  },
+  {
+    "revision": "84f0d948c63c77b1168b1dd5979d673a",
+    "url": "/SprintUofT/static/media/Logo.84f0d948.svg"
+  },
+  {
+    "revision": "a29f2c11f3aea7b3a4b8ce82f91d8495",
+    "url": "/SprintUofT/static/media/Pink_Cloud.a29f2c11.svg"
+  }
+]);
