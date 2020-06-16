8cb92197046559f320296c9ef765bc4f48be2430
Andy Jiang <andy.jiang99@outlook.com>
2020-06-15 22:11:22 -0400

    changed the currently in development placement

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import React from 'react';
 // import Email from './Icons/Email_Logo.svg';
 // import PinkCloud from './Icons/Pink_Cloud.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; 
+import './App.css';
 // import Container from 'react-bootstrap/Container';
 // import Col from 'react-bootstrap/Col';
 // import Row from 'react-bootstrap/Row';
 import Text from './components/home/introText';
-import {Container, Row, Col} from 'react-grid-system';
+import { Container, Row, Col } from 'react-grid-system';
 import './App.css';
 import ContactList from './components/contactList';
 
@@ -19,98 +19,101 @@ export default function App() {
   const isMobile = window.innerWidth <= 480;
   // const isLong = window.innerHeight> 820;
 
-  const pinkCloudStyle = (!isMobile) ? 
+  const pinkCloudStyle = (!isMobile) ?
     {
-      width: '100%', 
-      height: '100%', 
+      width: '100%',
+      height: '100%',
       position: 'absolute',
       bottom: '-5%',
       left: '-20%'
-    }:
+    } :
     {
-      width: '100%', 
+      width: '100%',
       height: '100%',
       marginLeft: '-20%'
     };
 
   const blueCloudStyle = (isMobile) ? {
     // mobile
-    width: '50%', 
+    width: '50%',
     height: 'auto',
     position: 'absolute',
     right: '-10%',
     top: '0%'
   } : {
-    width: '60%', 
-    height: '60%',
-    position: 'absolute',
-    top: '-20%',
-    right: '-10%'
-  }
+      width: '60%',
+      height: '60%',
+      position: 'absolute',
+      top: '-20%',
+      right: '-10%'
+    }
 
   const pinkCloud = (
-    <img  src={require('./Icons/Pink_Cloud.svg')} 
-          alt="board icon" 
-          style={pinkCloudStyle}/>
+    <img src={require('./Icons/Pink_Cloud.svg')}
+      alt="board icon"
+      style={pinkCloudStyle} />
   );
 
   const botRow = (isMobile) ? (
     //mobile
     <Row>
-      <Col md={5}>  <Text/> </Col>
-      
+      <Col md={5}>  <Text /> </Col>
+
       {/* <Col md={7}>  {pinkCloud} </Col> */}
     </Row>
-  ):(
-    //desktop
-    <Row>
-      <Col md={6}>  {pinkCloud} </Col>
-      <Col md={6}>  <Text/> </Col>
-    </Row>
-  );
+  ) : (
+      //desktop
+      <Row>
+        <Col md={6}>  {pinkCloud} </Col>
+        <Col md={6}>  <Text /> </Col>
+      </Row>
+    );
   const sloganFormat = (isMobile) ? {
     // mobile
     fontSize: '25px'
   } : {
-    fontSize: '50px'
-  }
+      fontSize: '43px'
+    }
 
   const development = {
+    position: 'absolute',
     fontSize: '9px',
-    textAlign: 'center'
+    left: '8%',
+    bottom: '-35px'
   }
 
   return (
-    <div className="App" style={{overflow: 'hidden'}}>    
-        <Container>
-         
-              <Row>
+    <div className="App" style={{ overflow: 'hidden' }}>
+      <Container>
+        <Row>
+          <img id="logo" src={require('./Icons/Logo.png')} alt="logo" />
+        </Row>
 
-                <img id="logo" src={require('./Icons/Logo.png')} alt="logo"/>
-              </Row>
-              
-              <Row>
-              <Col sm={7}>
-                <div className="title">
-                  <h1 className="slogan" style={sloganFormat}>
-                    Advancing Student 
+        <Row>
+          <Col sm={7}>
+            <div className="title">
+              <h1 className="slogan" style={sloganFormat}>
+                Advancing Student
                   </h1>
-                  <h1 className="slogan" style={sloganFormat}>
-                    Careers in Tech
+              <h1 className="slogan" style={sloganFormat}>
+                Careers in Tech
                   </h1>
 
-                    <ContactList
-                      facebook="https://www.facebook.com/SprintUofT/"
-                      // linkedin="https://www.google.ca"
-                      email="mailto:sprintuoft@gmail.com" />
-                </div>                
-              </Col>
-          </Row>
-          
-          <img src={require('./Icons/Blue_Cloud.svg')} alt="working icon" style={blueCloudStyle} />
-          {botRow}
-        </Container>
+              <ContactList
+                facebook="https://www.facebook.com/SprintUofT/"
+                // linkedin="https://www.google.ca"
+                email="mailto:sprintuoft@gmail.com" />
+            </div>
+          </Col>
+        </Row>
+
+        <img src={require('./Icons/Blue_Cloud.svg')} alt="working icon" style={blueCloudStyle} />
+        {botRow}
         <h4 style={development}>This page is currently under development.</h4>
-      </div>
-    );
+      </Container>
+      <br />
+      <br />
+      {/* <h4 style={development}>This page is currently under development.</h4> */}
+    </div>
+  );
 }
