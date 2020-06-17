85f88b6658afda103613e16d7a8b3af4efb43969
Andy Jiang <andy.jiang99@outlook.com>
2020-06-17 17:28:42 -0400

    Added target blank to links

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -5,17 +5,17 @@ export default function ContactList(props) {
     return (
     <ul className="contact-list">
         <li>
-            <a href={props.facebook}>
+            <a href={props.facebook} target="_blank" rel="noopener noreferrer">
             <img src={require('../Icons/Facebook_Logo.svg')} alt="facebook"/>
             </a>
         </li>
         <li>
-            <a href={props.linkedin}>
+            <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
             <img src={require('../Icons/LinkedIn_Logo.svg')} alt="linkedin"/>
             </a>
         </li>
         <li>
-            <a href={props.email}>
+            <a href={props.email} target="_blank" rel="noopener noreferrer">
             <img src={require('../Icons/Email_Logo.svg')} alt="email"/>
             </a>
         </li>
